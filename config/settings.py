a82549018bd4867cd6153cd369d00da825c68786
MKashkouli <m_kashkouli@yahoo.com>
2023-01-21 18:31:40 +0330

    add products app and model

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #my apps
+
+    #local apps
     'accounts',
     'pages',
+    "products",
+
 
     # Third party apps
     'crispy_forms',
