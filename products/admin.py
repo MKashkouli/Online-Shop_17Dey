d304607b094ff98b2ba1555a8ba93819445efa68
MKashkouli <m_kashkouli@yahoo.com>
2023-01-23 11:14:17 +0330

    add Listview and modified -base.html

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "active")
+    list_filter = ['active']
+    list_editable = ['active']
+    search_fields = ['title']
+    sortable_by= ['price']
